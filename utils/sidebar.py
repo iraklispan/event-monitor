9ceaba88ccfd5182956c639a2557a4f37bdc6df6
iraklispan <iraklispan@gmail.com>
2026-04-21 11:41:50 +0300

    Revert sidebar

--- utils/sidebar.py
+++ utils/sidebar.py
@@ -5,26 +5,19 @@ sys.dont_write_bytecode = True
 
 import streamlit as st
 from utils.navigation    import hide_default_nav_bar, nav_bar
-from utils.header_footer import app_title, copy_right, logo
+from utils.header_footer import app_title, copy_right
 from utils.buttons       import refresh_button
 from utils.auth          import logout_button
 
 
 def sidebar() -> None:
-    # Header
-    logo()
     app_title()
     st.sidebar.markdown("---")
-    
-    # Navigation
     hide_default_nav_bar()
     nav_bar()
     st.sidebar.markdown("---")
-    
-    # Buttons
     refresh_button()
+    st.sidebar.markdown("<br>", unsafe_allow_html=True)
     logout_button()
     st.sidebar.markdown("---")
-
-    # Footer
     copy_right()
